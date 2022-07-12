2a721fcf60c0ec44eccdd1465313aa2b48744530
Raphael Jungmair <raphael.jungmair@students.fhnw.ch>
2022-07-12 14:06:00 +0200

    To get Pepper working in API 7 added maven {             url 'https://qisdk.softbankrobotics.com/sdk/maven/'         } under dependencyResolutionManagement ind Setting.gradle file

--- app/src/main/java/com/example/peppermri/MainActivity.java
+++ app/src/main/java/com/example/peppermri/MainActivity.java
@@ -4,13 +4,31 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.design.activity.RobotActivity;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends RobotActivity implements RobotLifecycleCallbacks {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+        
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+
     }
 }
